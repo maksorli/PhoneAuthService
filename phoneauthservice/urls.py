60762e2e8149154dc205472cae410b9ded72951e
MaksimEvdokimov <maksorli@gmail.com>
2024-12-02 17:59:25 +0400

    Добавлен Redoc

--- phoneauthservice/urls.py
+++ phoneauthservice/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,5 +14,10 @@ urlpatterns = [
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
 ]
